aa3d38e715a501516600e89e9dd2b19a2006ed29
cédric  martin <cedricmartin@MacBook-Air.local>
2026-07-09 21:26:18 +0200

    feat(backend): init Spring Boot app, Configure PostgreSql, add health endpoint

---
+++ backend/src/test/java/com/cedmarti/jobtracking/JobtrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cedmarti.jobtracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
